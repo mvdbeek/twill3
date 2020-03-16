da24de32b6eb83ea0a93e76d887a43e3ef2f807e
mvdbeek <m.vandenbeek@gmail.com>
2020-03-16 11:32:03 +0100

    Change setup.py back to original author, license, version scheme etc

--- setup.py
+++ setup.py
@@ -4,19 +4,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='twill3',
-    version='0.11.2',
-    scripts=['twill'],
+    name='twill',
+    version='1.9.0',
+    scripts=['twill-sh'],
     packages=find_packages(),
-    author='Jon Froiland',
-    author_email='jon.froiland@gmail.com',
-    description='Update twill for Python3 compatibility',
-    long_description='Using 2to3, refactored code for Python3. Version references <version>.<month>.<change>',
+    author='C. Titus Brown and Ben R. Taylor',
+    author_email='titus@idyll.org',
+    license='MIT',
+    description='twill Web browsing language',
+    long_description='A scripting system for automating Web browsing. Useful for testing Web pages or grabbing data from password-protected sites automatically.',
     long_description_content_type='text/markdown',
-    url='',
+    url='https://github.com/ctb/twill',
     classifiers=[
         'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-    ], install_requires=['lxml', 'requests', 'pyparsing']
+    ], install_requires=['lxml', 'cssselect', 'requests', 'pyparsing']
 )
